32a720832d96c0f9528d2533d5079ba6f8e25102
Vixsa <victoriagiter@gmail.com>
2023-03-07 19:55:31 +0200

    первая задача

---
+++ 25/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число A - ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число B - ");
+int B = int.Parse(Console.ReadLine());
+double result = Math.Pow(A, B);
+Console.Write($"{result}");
